80fdb42454403af57b0e6ad55949609216f49c8d
h7k2 <keddousheytem@gmail.com>
2025-09-04 12:43:03 +0200

    Remove unnecessary newline prints in 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,12 +6,9 @@ number = random.randint(-10, 10)
 
 if number > 0:
     print(f"{number} is positive")
-    print("\n")
 
 elif number == 0:
     print(f"{number} is zero")
-    print("\n")
 
 else:
     print(f"{number} is negative")
-    print("\n")
