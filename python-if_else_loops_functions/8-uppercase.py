693e5073b6faec85abdd9feafa8bc3a92c020e05
h7k2 <keddousheytem@gmail.com>
2025-09-05 00:13:10 +0200

    Fix uppercase function definition and logic for converting lowercase letters

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def UpperCase(Str)
- for C in str:
-    if "a" < c < "z"
-        c = chr(ord(c) + 32
-    print("{}".format(C), end=""
- print()
+def uppercase(str):
+    for c in str:
+        if 'a' <= c <= 'z':
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print()
