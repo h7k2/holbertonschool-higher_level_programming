acf2bb3a453ab6c6c9deb6c465d6bcce740c6a92
h7k2 <keddousheytem@gmail.com>
2025-09-04 12:34:14 +0200

    Refactor output messages in 0-positive_or_negative.py to include the number in print statements

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,13 +3,12 @@
 import random
 
 number = random.randint(-10, 10)
-print(number)
 
 if number > 0:
-    print("number is greater than 0")
+    print(f"{number} is greater than 0")
 
 elif number == 0:
-    print("number is 0")
+    print(f"{number} is 0")
 
 else:
-    print("number is less than 0")
+    print(f"{number} is less than 0")
