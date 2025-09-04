01a3b185ce1eeb647bd056081d98a404c3186dc2
h7k2 <keddousheytem@gmail.com>
2025-09-04 12:40:58 +0200

    Fix newline placement in output messages of 0-positive_or_negative.py

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -6,9 +6,12 @@ number = random.randint(-10, 10)
 
 if number > 0:
     print(f"{number} is positive")
+    print("\n")
 
 elif number == 0:
     print(f"{number} is zero")
+    print("\n")
 
 else:
     print(f"{number} is negative")
+    print("\n")
