5d303106c05032797e0c826ef8f95488e40ca643
h7k2 <keddousheytem@gmail.com>
2025-09-05 16:44:13 +0200

    Fix type conversion in infinite addition script

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ import sys
 if __name__ == "__main__":
     total = 0
     for i in range(1, len(sys.argv)):
-        total = total + sys.argv[i]  # ❌ oubli de int()
+        total += int(sys.argv[i])
     print("{}".format(total))
