21f5a0a90292424bbf0aa54cd6c30ee51796f5c3
h7k2 <keddousheytem@gmail.com>
2025-09-05 12:37:23 +0200

    Refactor print_last_digit function to simplify logic and remove unnecessary condition

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number = number * -1
     last = number % 10
     print(last, end="")
-    return last + 0
+    return last
