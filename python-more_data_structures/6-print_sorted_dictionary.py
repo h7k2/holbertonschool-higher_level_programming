ed0f4fc5c15a586df620bc5a4f7337d29d00ed3e
h7k2 <keddousheytem@gmail.com>
2025-09-10 20:38:39 +0200

    Fix print_sorted_dictionary function to correctly iterate over dictionary keys and print key-value pairs

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for val in a_dictionary.values():
-        print(val, ":", a_dictionary)
-    return a_dictionary.keys
+    for key in a_dictionary:
+        print(key, ":", a_dictionary[key])
+    return a_dictionary
