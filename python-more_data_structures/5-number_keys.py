d8af43d8969393b081a6081ecb0d39f1fc23b796
h7k2 <keddousheytem@gmail.com>
2025-09-10 18:47:49 +0200

    Fix number_keys function to correctly count dictionary keys

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    count = "0"
-    for val in a_dictionary:
-        count = count + 1
+    count = 0
+    for val in a_dictionary.values():
+        count += 1
     return a_dictionary
