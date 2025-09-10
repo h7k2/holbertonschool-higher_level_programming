8acd28267c4b60829623bd26d865674fb6ac1c90
h7k2 <keddousheytem@gmail.com>
2025-09-10 13:56:12 +0200

    Fix indentation error in divisible_by_2 function

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,6 +4,6 @@ def divisible_by_2(my_list=[]):
     for val in my_list:
         if val % 2 == 0:
             new_list.append(True)
-            else
-             new_list.append(False)
+        else:
+            new_list.append(False)
     return new_list
