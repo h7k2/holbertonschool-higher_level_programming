2a4c80b939ca9859bd1a70b5a41bcaa79eb950bb
h7k2 <keddousheytem@gmail.com>
2025-09-11 00:03:57 +0200

    Fix safe_print_list function to correctly count printed elements and handle exceptions

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,10 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        for i in range(0, x + 1):
+        for i in range(x):
             print(my_list[i], end="")
-            count = count - 1
+            count += 1
         print()
         return "count"
     except:
-        return
+        return 0
