f9869ba8c9e87b546f0989dee6adcd4a439014bc
h7k2 <keddousheytem@gmail.com>
2025-09-10 15:26:25 +0200

    Fix indentation in square_matrix_simple.py to ensure proper functionality

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,5 @@ def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
         new_row = [val ** 2 for val in row]
+        new_matrix.append(new_row)
     return new_matrix
