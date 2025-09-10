52fbd6f9ac4f51a3e8de949fcaa07de00b0a0e03
h7k2 <keddousheytem@gmail.com>
2025-09-10 21:25:20 +0200

    Fix multiply_by_2 function to return a new dictionary with doubled keys

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
+    new_dict = {}
     for key in a_dictionary:
-        a_dictionary[key] = key * 2
-    return key
+        new_dict[key] = key * 2
+    return new_dict
