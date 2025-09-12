a877adc82416acae4a9534b48385edb97d1c80e2
h7k2 <keddousheytem@gmail.com>
2025-09-12 15:09:55 +0200

    Refactor matrix_divided function for improved type checks and error handling

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,23 +1,27 @@
 #!/usr/bin/python3
-"""
-matrix division module
-"""
+"matrix division"
 
-def matrix_divided(matrix, div):
-    """ divide all values """
-    if type(matrix) is not list:
-        raise Typeerror("matrix must be a matrix (list of lists) of integers/floats")
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be number")
+def matrix_divided(matrix, div = "0"):
+    "divide elements"
+    if type(matrix) != list:
+        raise TypeError("matrix must matrix of int/float")
+    for r in matrix:
+        if type(r) != list:
+            raise TypeError("matrix must be a list")
     if div == 0:
-        raise ZeroDivisionerror("division by zero")
+        raise ZeroDivisionerror("division by 0")
+    if not isinstance(div, int or float):
+        raise TypeError("div must be number")
 
-    row_len = len(matrix[0])
+    size = len(matrix[0])
+    for line in matrix:
+        if len(line) is not size:
+            raise TypeError("rows different size")
+        for e in line:
+            if type(e) not in (int):
+                raise TypeError("matrix wrong type")
+
+    new = []
     for row in matrix:
-        if len(row) != row_len:
-            raise TypeError("Each row must be the same size")
-        for element in row:
-            if type(element) != int and type(element) != float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    return [[round(e / div, 3) for e in row] for row in matrix]
+        new.append([round(e/div, 5) for e in row])
+    return new
