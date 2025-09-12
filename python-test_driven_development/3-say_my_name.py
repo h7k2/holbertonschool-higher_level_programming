1f2b685f07cfcde16f840f4942894a019f443602
h7k2 <keddousheytem@gmail.com>
2025-09-12 16:52:10 +0200

    Refactor say_my_name function: update module docstring and fix last_name error message

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """
-This module defines `say_my_name` to print a formatted string.
-It checks that first_name and last_name are strings.
+say my name module
 """
 
 def say_my_name(first_name, last_name=""):
     """
-    Print "My name is <first name> <last name>".
-
-    Raises:
-        TypeError: if first_name or last_name are not strings
+    print my name
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
+        raise TypeError("lastname must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {}{}".format(first_name, last_name))
