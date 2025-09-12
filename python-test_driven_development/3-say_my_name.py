31c6f76c61aa56618944a1568ebc59a4c38489b2
h7k2 <keddousheytem@gmail.com>
2025-09-12 16:48:17 +0200

    Refactor say_my_name function: enhance docstring, fix type checks, and improve formatting

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """
-say my name module
+This module defines `say_my_name` to print a formatted string.
+It checks that first_name and last_name are strings.
 """
 
-def say_my_name(first_name, last_name=' '):
-    """print my name"""
-    if type(first_name) != str:
-        raise Typeerror("first_name must be a string")
-    if not isinstance(last_name, str):
-        raise TypeError("lastname must be a string")
+def say_my_name(first_name, last_name=""):
+    """
+    Print "My name is <first name> <last name>".
 
-    print("My name is {}{}".format(first_name, last_name))
+    Raises:
+        TypeError: if first_name or last_name are not strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
