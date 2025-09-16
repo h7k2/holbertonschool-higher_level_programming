3329242ea0f4313ea708bbbee5157b3a2f768818
h7k2 <keddousheytem@gmail.com>
2025-09-16 21:06:39 +0200

    Add initial implementation of Square class in 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines a class Square with a private instance attribute: size
+"""
+
+
+class Square:
+    """
+    Class Square that defines a square by its size
+    """
+
+    def __init__(self, size):
+        pass
