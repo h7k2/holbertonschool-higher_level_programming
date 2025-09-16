897b4949e1015af8dfa79ab1194eea0a6e849c2b
h7k2 <keddousheytem@gmail.com>
2025-09-16 12:17:08 +0200

    Fix docstring formatting in 0-square.py for clarity

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-'''0-square.py'''
-'''Empty class Square that defines a square'''
+'''0-square.py
+Empty class Square that defines a square'''
 
 class Square:
     """This is an empty class that defines a square"""
