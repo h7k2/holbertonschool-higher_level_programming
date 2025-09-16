29a9aeb05b57d99531a731bbc8ee76b3a7e09047
h7k2 <keddousheytem@gmail.com>
2025-09-16 21:10:36 +0200

    Refactor docstrings in 1-square.py for improved formatting and clarity

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Module 1-square
-Defines a class Square with a private instance attribute: size"""
+"""
+Module 1-square
+Defines a class Square with a private instance attribute: size
+"""
 
 
 class Square:
-    """Class Square that defines a square by its size"""
+    """
+    Class Square that defines a square by its size
+    """
 
     def __init__(self, size):
-          """Initialize a new Square instance.
+        """
+        Initialize a new Square instance.
 
         Args:
-            size: The size of the square (no type/value verification here)."""
+            size: The size of the square (no type/value verification here).
+        """
         self.__size = size
