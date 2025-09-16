15024b6b83fc7e608ba1644398a56af39e79fc3b
h7k2 <keddousheytem@gmail.com>
2025-09-17 00:49:49 +0200

    Add area method to Square class for area computation

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -35,3 +35,21 @@ class Square:
 
     @size.setter
     def size(self, value):
+        """
+        Setter for the size of the square.
+
+        Args:
+            value (int): new size of the square
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+ 
