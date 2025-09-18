4647937fa178e74bfc3861397e7aa01aec0bda45
h7k2 <keddousheytem@gmail.com>
2025-09-18 14:14:11 +0200

    Add Rectangle class definition

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 0-rectangle
+Defines an empty class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class that defines a rectangle
+    """
+    pass
