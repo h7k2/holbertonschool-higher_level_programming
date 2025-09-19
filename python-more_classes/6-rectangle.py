94fb0bfac4afeebe195015165bd5b027b5e5d58f
h7k2 <keddousheytem@gmail.com>
2025-09-19 11:19:26 +0200

    Refactor Rectangle class: improve setter validation and optimize string representation

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -11,22 +11,22 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -34,25 +34,23 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
-    
+
     def perimeter(self):
         if self.__width == 0 or self.__height == 0:
             return 0
-        return (2 * self.__width) + (2 * self.__height)
+        return 2 * self.__width + 2 * self.__height
 
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        r = ""
+        lines = []
         for i in range(self.__height):
-            r += "#" * self.__width
-            if i < self.__height - 1:
-                r += "\n"
-        return r
+            lines.append("#" * self.__width)
+        return "\n".join(lines)
 
     def __repr__(self):
         return "Rectangle({}, {})".format(self.__width, self.__height)
-    
+
     def __del__(self):
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
