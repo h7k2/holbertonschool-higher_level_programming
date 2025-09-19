7322a972c2be5b0e29187a58097595a40755e6c3
h7k2 <keddousheytem@gmail.com>
2025-09-19 11:17:51 +0200

    Fix typos and improve initialization in Rectangle class

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_instances = Rectangle.number_of_instances + 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -14,7 +14,7 @@ class Rectangle:
     
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -22,36 +22,36 @@ class Rectangle:
     
     @property
     def height(self):
-        return self.__heigt
+        return self.__height
     
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__heigt = value
+        self.__height = value
 
     def area(self):
-        return self.__width * self.__heigt
+        return self.__width * self.__height
     
     def perimeter(self):
-        if self.__width == 0 or self.__heigt == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * self.__width + 2 * self.__heigt
+        return (2 * self.__width) + (2 * self.__height)
 
     def __str__(self):
-        if self.__width == 0 or self.__heigt == 0:
+        if self.__width == 0 or self.__height == 0:
             return ""
         r = ""
-        for i in range(self.__heigt):
+        for i in range(self.__height):
             r += "#" * self.__width
-            if i != self.__heigt - 1:
+            if i < self.__height - 1:
                 r += "\n"
         return r
 
     def __repr__(self):
-        return "Rectangle(" + str(self.__width) + ", " + str(self.__heigt) + ")"
+        return "Rectangle({}, {})".format(self.__width, self.__height)
     
     def __del__(self):
         print("Bye rectangle...")
