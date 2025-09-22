ba31f57edf0ebaf7e8dcb3356ea797d8d8e0ee4d
h7k2 <keddousheytem@gmail.com>
2025-09-23 01:05:03 +0200

    Fix comparison operator in is_same_class function for accurate type checking

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This module defines the function is_same_class
 """
@@ -9,4 +8,4 @@ def is_same_class(obj, a_class):
     """
     Returns True if obj is exactly an instance of a_class, otherwise False
     """
-    return type(obj) == a_class
+    return type(obj) is a_class
