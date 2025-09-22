f821479315bd4017e91c2ba7445551f632aafca5
h7k2 <keddousheytem@gmail.com>
2025-09-22 22:44:05 +0200

    Enhance module docstring for lookup function to clarify purpose

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines the function lookup
+"""
+
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+    """
     return dir(obj)
