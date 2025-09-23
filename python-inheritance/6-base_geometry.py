1019efb89f719d087cd53c58fb32d71f20816099
h7k2 <keddousheytem@gmail.com>
2025-09-23 14:56:01 +0200

    Add BaseGeometry class definition in 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
