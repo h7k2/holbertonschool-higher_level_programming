d4004b6202913b8f7bdafad53974e8be312c18ff
h7k2 <keddousheytem@gmail.com>
2025-09-24 18:52:46 +0200

    Fix formatting by adding a blank line before the Rectangle class definition

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,9 +3,9 @@
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle defined by width and height."""
-
 
     def __init__(self, width, height):
         """Initialize a Rectangle after validating inputs."""
