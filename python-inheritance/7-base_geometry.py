8b03af93304dd2983acdb22d6790fce575e9eb56
h7k2 <keddousheytem@gmail.com>
2025-09-24 17:32:46 +0200

    Fix syntax errors in area and integer_validator methods in BaseGeometry class

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 class BaseGeometry:
-    def area(self)
-        raise Exeption("area() is not implemented")
+    def area(self):
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if value is not int:
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
-        if value <= 0
-            raise ValueError(f"{name}must be greater than 0")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
