7ffea6fd8b4466ad963fd8e82451c4a3a22a9920
h7k2 <keddousheytem@gmail.com>
2025-09-29 15:26:42 +0200

    Add from_json_string function to return JSON representation of a string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a function that returns the JSON representation of an string."""
+import json
+
+
+def from_json_string(my_str):
+     """return the JSON representation of as a string"""
+     return json.str(my_str)
