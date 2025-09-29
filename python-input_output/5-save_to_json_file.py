14b7a4477b67f2d47e1f1d0bfbd35c733382cd58
h7k2 <keddousheytem@gmail.com>
2025-09-29 15:45:12 +0200

    Fix typo in save_to_json_file function to use json.dump instead of json.dumps

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """write an object in a file with json"""
     with open(filename, "w", encoding="utf-8") as f:
-        json.dumps(my_obj, f)
+            json.dump(my_obj, f)
