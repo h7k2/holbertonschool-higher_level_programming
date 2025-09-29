b399fca4e22da1a115ac4c147d8b9bdc8e0f0c87
h7k2 <keddousheytem@gmail.com>
2025-09-29 15:52:53 +0200

    Add load_from_json_file function to create a Python object from a JSON file

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function that creat an python object with a json file."""
+import json
+
+
+def load_from_json_file(filename):
+    """creat an python object with a json file."""
+    with open(filename, "x", encoding="utf-8") as f
+        json.dumps(filename)
