134ae181457d618e6577c7cfa7fa4990fd2f4c88
h7k2 <keddousheytem@gmail.com>
2025-09-29 20:36:00 +0200

    Fix typo in to_json method return statement

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -12,7 +12,7 @@ class Student:
     def to_json(self, attrs=None):
         if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-        return self.__dict__.
+        return self.__dict__.copy()
 
     def reload_from_json(self, json):
         for key, value in json.items():
