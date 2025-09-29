5a3b3282508b295bcebc231ae63fa58d2150810a
h7k2 <keddousheytem@gmail.com>
2025-09-29 20:05:16 +0200

    Fix class definition and constructor in 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+"""a class Student that defines a studen"""
 
+
+class student:
 
 def __init__(self, first_name, last_name, age):
-
-class _init_ student(first_name, last_name, age):
     self.first_name = first_name
     self.last_name = last_name
     self.age = age
-        def json(self):
-            return self.__dict__.
+
+def json(self):
+    return self.__dict__.
