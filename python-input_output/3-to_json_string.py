0ed02024856078e34bd9b8b2475870ad1626844f
h7k2 <keddousheytem@gmail.com>
2025-09-29 15:18:25 +0200

    Refactor docstring and import statement in to_json_string function for clarity

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-
-
+"""a function that returns the JSON representation of an object."""
 import json
+
+
 def to_json_string(my_obj):
+    """return the JSON representation of an object as a string"""
     return json.dumps(my_obj)
