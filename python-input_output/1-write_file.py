079f1713485492a90d3821e384b2a541df540cef
h7k2 <keddousheytem@gmail.com>
2025-09-29 14:27:25 +0200

    Fix docstring and syntax errors in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""A function that writes a string to a text file (UTF8) 
-and returns the number of characters written"""
+"""A function that writes a string to a text file (UTF8)"""
+
 
 def write_file(filename="", text=""):
     with open(filename, "w", encoding utf -8) as f:
-        print(f.write(), end")
+        print f.write(text)
