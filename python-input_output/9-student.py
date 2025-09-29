c2117907bff471a1fd2b06e8d81a73592ee71224
h7k2 <keddousheytem@gmail.com>
2025-09-29 20:05:58 +0200

    Fix indentation for class methods in 9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,10 +4,10 @@
 
 class student:
 
-def __init__(self, first_name, last_name, age):
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-def json(self):
-    return self.__dict__.
+    def json(self):
+        return self.__dict__.
