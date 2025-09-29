0815263e92318026a7154c34ac195d131d1a5bc0
h7k2 <keddousheytem@gmail.com>
2025-09-29 15:40:22 +0200

    Add save_to_json_file function to serialize an object to a JSON file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""a function that write an object in a file with json."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """write an object in a file with json"""
+    with open(filename, "w", encoding=utf-8) as f:
+        json.dumps(my_obj, f)
