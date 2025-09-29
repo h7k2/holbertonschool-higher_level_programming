8c23107339e0c6b5d0a81bc148f76bfd59e325bb
h7k2 <keddousheytem@gmail.com>
2025-09-29 20:07:15 +0200

    Update docstrings for student class and methods for clarity

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,11 +3,14 @@
 
 
 class student:
+    """defines a student."""
 
     def __init__(self, first_name, last_name, age):
+        """new Student instance"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def json(self):
+        """representation of a Student instance"""
         return self.__dict__.
