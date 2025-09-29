1aa6264644bf675c7fcd70d0bd8a53871b0eeea5
h7k2 <keddousheytem@gmail.com>
2025-09-29 14:30:56 +0200

    Fix encoding declaration and update module docstring for clarity

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""A function that writes a string to a text file (UTF8)"""
+"""Module for a function that writes a string to a UTF-8 text file."""
 
 
 def write_file(filename="", text=""):
     """Write a string to a UTF-8 text file and return number of characters"""
-    with open(filename, "w", encoding utf -8) as f:
+    with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
