93ccab90c6e3b41fe138d9b8b87de84f8a1d6224
h7k2 <keddousheytem@gmail.com>
2025-09-29 14:42:06 +0200

    Add append_write function to append a string to a text file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a function that appends a string at the end of a text file (UTF8) """
+
+
+def append_write(filename="", text=""):
+    """add som at the end"""
+    with open(filenamle, "a", encoding="utf-8") as f:
