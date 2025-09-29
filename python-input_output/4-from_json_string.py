139a30adf051b72cfab9c9dd18ec22afc1394957
h7k2 <keddousheytem@gmail.com>
2025-09-29 15:32:49 +0200

    Fix indentation in from_json_string function docstring

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -4,5 +4,5 @@ import json
 
 
 def from_json_string(my_str):
-     """return the JSON representation of a string"""
-     return json.loads(my_str)
+    """return the JSON representation of a string"""
+    return json.loads(my_str)
