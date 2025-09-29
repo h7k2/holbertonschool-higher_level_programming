513eaf021ae8ac9ea4983bf441e968dff8c7e52a
h7k2 <keddousheytem@gmail.com>
2025-09-29 14:30:19 +0200

    Fix syntax error in write_file function by correcting encoding declaration

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -3,5 +3,6 @@
 
 
 def write_file(filename="", text=""):
+    """Write a string to a UTF-8 text file and return number of characters"""
     with open(filename, "w", encoding utf -8) as f:
         return f.write(text)
