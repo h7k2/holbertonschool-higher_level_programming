0b18a2b0374369ec5cc6b1d8c23d6a7e20bbadf0
h7k2 <keddousheytem@gmail.com>
2025-09-29 15:01:07 +0200

    Refactor docstrings for consistency and clarity in append_write function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""a function that appends a string at the end of a text file (UTF8) """
+"""Module for a function that appends a string to a UTF-8 text file."""
 
 
 def append_write(filename="", text=""):
-"""append a string to a UTF-8 text file and return number of characters"""
+    """Append a string to a UTF-8 text file and return number of characters"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
