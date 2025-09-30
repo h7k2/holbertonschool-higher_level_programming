0d056811945ffc731f38f1e3bcfefc3638b67b22
h7k2 <keddousheytem@gmail.com>
2025-09-30 10:56:09 +0200

    Fix syntax errors and improve error messages in serialization functions

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,20 +1,21 @@
-#!/usr/bin/python3
-"""adds the functionality to serialize a Python dictionary to a JSON file"""
+#!/usr/bin/env python3
+"""adds the functionality to serialize a Python dictionary to a JSON file."""
 import json
 
 
 def serialize_and_save_to_file(data, filename):
     if not isinstance(data, dict):
-        raise TypeError
+        raise TypeError("data must be a dictionary")
 
-    with open(filename, "w", encoding "utf-8") as f:
-        json.dump(data, file, indent=2, ensure_ascii=False)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
 
 def load_and_deserialize(filename):
-    with open(filename, "r", encoding "utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         obj = json.load(f)
 
     if not isinstance(obj, dict):
-        raise TypeError
+        raise TypeError("this does not contain a dictionary")
 
     return obj
