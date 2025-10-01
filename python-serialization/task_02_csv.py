b1831c8794f1f38f3477231279580bf034bf61f9
h7k2 <keddousheytem@gmail.com>
2025-10-01 15:45:38 +0200

    Fix syntax errors in CSV to JSON conversion function

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,3 +7,11 @@ def convert_csv_to_json(filename):
     with open(filename, "r", eading="utf-8") as csv.DictReader
      reader = csv.DictReader(csv_file)
             data = list(reader)
+    
+      with open("data.json", "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, indent=4)
+    
+    return True
+
+else 
+    return False
