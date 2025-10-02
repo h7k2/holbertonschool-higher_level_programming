0d05efbf8c5a244c74b8460aa99ebb1ad644c20f
h7k2 <keddousheytem@gmail.com>
2025-10-02 10:53:36 +0200

    Fix serialization function to use correct variable names and ensure XML output

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -8,3 +8,12 @@ def serialize_to_xml(dictionary, filename):
     for key, value in _dict_
         child = ET.SubElement(root, key)
         child.text = str(value)
+    
+    tree = ET.ElementTree(root)
+    tree.write(filename)
+
+
+def deserialize_from_xml(filename):
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
