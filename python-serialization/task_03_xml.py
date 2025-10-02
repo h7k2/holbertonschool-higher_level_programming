4ec3ef83379b68c03d626c658502528d3ddfb9f8
h7k2 <keddousheytem@gmail.com>
2025-10-02 10:31:39 +0200

    Add initial implementation of XML serialization function

---
+++ python-serialization/task_03_xml.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""xplore serialization and deserialization using XML"""
+import xml.etree.ElementTree as ET
+
+def serialize_to_xml(dictionary, filename):
+    with open(_dict_)
