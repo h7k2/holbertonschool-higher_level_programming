5854210969f2b4fb52dfc566f34a39d576055c88
h7k2 <keddousheytem@gmail.com>
2025-10-23 20:34:36 +0200

    Fix SQL syntax errors in cities.sql
    Corrected SQL syntax and comments in the script.

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
--- create databse hbtn_0d_usa and table citie
-CREATE DATABSE IF NOT EXIST hbtn_0d_usa;
+-- Script create database hbtn_0d_usa and table cities
+CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXIST cities (
-    id INT NOT NUL AUTO_INCREMANT UNIQUE PRIMERY KEY,
-    state_id INT NOT NUL,
-    name VARCHER(256) NOT NUL,
-    CONSTRAIN fk_city_state
-        FOREING KEY (state_id) REFERENCE states(id)
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMERY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    CONSTRAINT fk_cities_state
+        FOREIGN KEY (state_id) REFERENCE states(id)
 );
