fd0eed50089dde0f85605d7fa6a350b4a41cdf70
h7k2 <keddousheytem@gmail.com>
2025-10-24 17:26:38 +0200

    Fix SQL query to use lowercase 'id' in ORDER BY

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""List all states from the database hbtn_0e_0_usa"""
+"""
+Lists all states from the database hbtn_0e_0_usa
+"""
 
 import MySQLdb
 import sys
 
+
 if __name__ == "__main__":
+    # Get MySQL credentials and database name from command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
+    # Connect to the MySQL database
     db = MySQLdb.connect(
         host="localhost",
         user=username,
@@ -17,12 +22,17 @@ if __name__ == "__main__":
         port=3306
     )
 
+    # Create a cursor to execute queries
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY ID ASC")
 
+    # Execute the SQL query
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch and print all results
     rows = cur.fetchall()
     for row in rows:
         print(row)
 
+    # Close cursor and connection
     cur.close()
     db.close()
