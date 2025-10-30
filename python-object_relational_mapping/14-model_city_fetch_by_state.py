8afb3e2b027b882c8c847dc6e8acc6db3280d173
h7k2 <keddousheytem@gmail.com>
2025-10-30 11:31:13 +0100

    Add City model and script to fetch City objects from the database

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Prints all City objects from the database hbtn_0e_14_usa
+Format: <state name>: (<city id>) <city name>
+Sorted by cities.id ascending
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost:3306/{database}",
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Join State -> City, order by City.id
+    rows = (
+        session.query(State.name, City.id, City.name)
+        .join(City, City.state_id == State.id)
+        .order_by(City.id.asc())
+        .all()
+    )
+
+    for state_name, city_id, city_name in rows:
+        print(f"{state_name}: ({city_id}) {city_name}")
+
+    session.close()
